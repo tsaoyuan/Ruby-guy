4fe82f455b5605d8390b88930b616710ae4c1883
tsao <tsaoyuan@gmail.com>
2022-04-27 23:21:26 +0800

    attr_writer 取代 Setter mathods
    - 定義 setter methods 起手式 attr_writer :a-method, :b-method, ...等
    - colon + name (Ruby 特有的方法表示方式; symbol; :a-method)

--- BootrAils_attr_reader.rb
+++ BootrAils_attr_reader.rb
@@ -2,9 +2,12 @@
 # 改用 attr_reader 取代 Getter mathods
 # 程式碼簡潔易讀
 
+# 既然 Getter mathods 有 attr_reader，解省寫法
+# Setter methods 也有 attr_writer 可以使用
 class Book  
   
   attr_reader :title, :author # <-- getter methods 
+  attr_writer :title, :author # <-- setter methods
 
   def initialize(title, author)  
     @title = title  
@@ -15,4 +18,5 @@ end
 
 harry = Book.new("wonderful","Divid, Wong")
 p harry
+harry.title = "GiGi"
 puts harry.title
